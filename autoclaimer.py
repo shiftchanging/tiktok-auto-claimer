8b8f48c49621785b67cbcbf89e12893cd9c8f5d1
shiftchanging <93018013+shiftchanging@users.noreply.github.com>
2021-11-26 10:59:32 +0000

    Add files via upload

---
+++ autoclaimer.py
@@ -0,0 +1,804 @@
+import time
+import webbrowser
+from time import sleep
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+import urllib
+
+import ctypes
+ctypes.windll.kernel32.SetConsoleTitleW("shiftchanging on tiktok.com")
+
+from os import system
+system("title " + "shiftchanging on tiktok")
+
+print("""
+     _.-^^---....,,--       
+ _--                  --_  
+<                        >)
+|                         | 
+ \._                   _./  
+    ```--. . , ; .--'''       
+          | |   |             
+       .-=||  | |=-.   
+       `-=#$%&%$#=-'   
+          | ;  :|     
+ _____.,-#%&$@%#&#~,._____
+""")
+
+print(f"{Fore.RED}wait 5 seconds to open program")
+
+print(f"{Fore.RED}5")
+sleep(1)
+print(f"{Fore.RED}4")
+sleep(1)
+print(f"{Fore.RED}3")
+sleep(1)
+print(f"{Fore.RED}2")
+sleep(1)
+print(f"{Fore.RED}1")
+sleep(1)
+
+
+print("""
+
+███████╗██╗  ██╗██╗███████╗████████╗███████╗     ██████╗██╗      █████╗ ██╗███╗   ███╗███████╗██████╗ 
+██╔════╝██║  ██║██║██╔════╝╚══██╔══╝██╔════╝    ██╔════╝██║     ██╔══██╗██║████╗ ████║██╔════╝██╔══██╗
+███████╗███████║██║█████╗     ██║   ███████╗    ██║     ██║     ███████║██║██╔████╔██║█████╗  ██████╔╝
+╚════██║██╔══██║██║██╔══╝     ██║   ╚════██║    ██║     ██║     ██╔══██║██║██║╚██╔╝██║██╔══╝  ██╔══██╗
+███████║██║  ██║██║██║        ██║   ███████║    ╚██████╗███████╗██║  ██║██║██║ ╚═╝ ██║███████╗██║  ██║
+""")
+
+              #This tool autoclaims a user on tiktok when being changed
+              #Have fun
+              #BTW IF YOUR LOOKING AT THE CODE THIS A JOKE DOESNT ACC WORK LOL
+              #troll your friends with it and tiktok and tag me @shiftchangings auto claimer
+              #only took me 10 minutes LOLL
+
+
+webbrowser.open("https://www.tiktok.com/@shiftchanging")
+webbrowser.open("https://github.com/shiftchanging/")
+
+print(" ")
+
+
+
+print(f"{Fore.RED}THIS ONLY WORKS WHEN A USER IS BEING CHANGED ON TIKTOK AND WILL CLAIM THAT USER FAST")
+print(" ")
+print(f"{Fore.RED}This program was made by shiftchanging")
+print(" ")
+
+user = input(f"{Fore.RED}What is your username that you want to claim? ")
+print(" ")
+
+print(f"{Fore.RED}ENTER USERNAME")
+print(" ")
+
+print(f"{Fore.GREEN}-CLAIMING-", user)
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-", user)
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-",user)
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-", user)
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-", user)
+print(f"{Fore.RED}STILL WORKING", user)
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-",user)
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-", user)
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-", user)
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-", user)
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}-IP BLOCKED-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}-IP BLOCKED-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}-IP BLOCKED-")
+sleep(0.5)
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}-IP BLOCKED-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}-IP BLOCKED-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}-IP BLOCKED-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}STILL WORKING")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+print(f"{Fore.RED}-IP BLOCKED-")
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-", user)
+sleep(0.5)
+
+print(f"{Fore.GREEN}-CLAIMING-")
+
+
+
+
+
